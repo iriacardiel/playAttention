dca15e94492a9d8a7a2b643ff5bdad8e1b11f773
iriacardiel <iriacardielperez@gmail.com>
2025-07-10 12:01:10 +0200

    tokenization module

--- README.md
+++ README.md
@@ -3,3 +3,20 @@ A playground to understand Attention and Transformers.
 Based on: 
 
 - [Alfredo Canziani class on Attention and Transformers](https://www.youtube.com/watch?v=fEVyfT-gLqQ&t=828s)
+- [Let's build GPT: from scratch, in code, spelled out](https://www.youtube.com/watch?v=kCc8FmEb1nY&list=PLAqhIrjkxbuWI23v9cThsA9GvCAUhRvKZ&index=7)
+
+
+# Input data
+
+https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+
+From terminal:
+```
+curl -o input.txt https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+```
+
+From jupyter notebook:
+
+```
+!wget https://www.youtube.com/watch?v=kCc8FmEb1nY&list=PLAqhIrjkxbuWI23v9cThsA9GvCAUhRvKZ&index=7
+```
---
+++ tokenization/tokenizers.py
@@ -0,0 +1,39 @@
+import tiktoken
+
+# Load text file
+with open('data/tinyshakespeare.txt', 'r') as f:
+    train_text = f.read()
+    
+# CHARACTER LEVEL TOKENIZERS
+# --------------------------
+# (option 0) Self implementation of character level tokenizer ("no training" needed)
+class CharTokenizer:
+    def __init__(self, text):
+        self.text = text
+        self.vocab = sorted(list(set(text)))
+        self.vocab_size = len(self.vocab)
+        self.char_to_int = {ch: i for i, ch in enumerate(self.vocab)}
+        self.int_to_char = {i: ch for i, ch in enumerate(self.vocab)}
+
+    def encode(self, s):
+        return [self.char_to_int[ch] for ch in s]
+
+    def decode(self, l):
+        return ''.join([self.int_to_char[i] for i in l])
+    
+char_level_tokenizer = CharTokenizer(train_text)
+
+
+# WORD LEVEL TOKENIZERS (BPE)
+# --------------------------
+# (option 1) Self implementation of Byte Pair Encoding (BPE) tokenizer in playBPE repository (https://github.com/iriacardiel/playBPE) (needs training to merge byte pairs)
+# (option 2) Loading the tiktoken tokenizer for GPT-2 (already trained based on BPE)
+tiktoken_tokenizer = tiktoken.get_encoding("gpt2")
+
+
+if __name__ == "__main__":
+    tokenizer = tiktoken_tokenizer 
+    encoded = tokenizer.encode("hello world")
+    decoded = tokenizer.decode(encoded)
+    print(f"Encoded: {encoded}")
+    print(f"Decoded: {decoded}")
